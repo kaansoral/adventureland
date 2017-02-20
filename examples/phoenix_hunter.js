f0ede3270c10a6bacedffe6e477f5d753ade13c1
Kaan Soral <kaansoral@gmail.com>
2017-02-20 13:29:01 +0300

    Update phoenix_hunter.js

--- examples/phoenix_hunter.js
+++ examples/phoenix_hunter.js
@@ -4,13 +4,14 @@ setInterval(function(){
 	loot();
 
 	if(is_moving(character)) return;
-	// IDEA: Instead of returning when is_moving, if character.targets<2, farm nearby monsters if they are in_attack_range
+	//IDEA: Instead of returning when is_moving, if character.targets<2, farm nearby monsters if they are in_attack_range
 
 	var target=get_nearest_monster({type:"phoenix"});
 	if(!target)
 	{
 		smart_move({to:"phoenix"},function(r){
 			if(r) game_log("At another Phoenix spawn!","#E0C34C");
+			//TODO: "tunnel" is a dangerous map, find a corner and pull Phoenix there, if a mole targets you, use("town") and escape, heal, re-try
 		});
 		return;
 	}
