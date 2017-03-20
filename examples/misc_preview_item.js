ec63cc53c4a764a130aa9a0fc73dd8810ab4d4eb
Kaan Soral <kaansoral@gmail.com>
2017-03-20 10:09:24 +0300

    Create misc_preview_item.js

---
+++ examples/misc_preview_item.js
@@ -0,0 +1,6 @@
+var new_staff=G.items.staff;
+new_staff.dex=10
+new_staff.upgrade.dex=5
+new_staff.name="Swift Staff"
+new_staff.explanation="It gets swifter and swifter!"
+preview_item(new_staff,{thumbnail:false});
