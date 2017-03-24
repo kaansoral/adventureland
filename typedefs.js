234bbb42dd5aec87db5e420dd60db61bf15c5b83
Patric Wellershaus <p.wellershaus@googlemail.com>
2017-03-24 18:57:59 +0100

    First Commit with partial JsDoc

---
+++ typedefs.js
@@ -0,0 +1,64 @@
+/**
+ * Created by nexus on 24/03/17.
+ */
+/**
+ * @typedef {Object} ItemStats
+ * @property {number} apiercing
+ * @property {number} armor
+ * @property {number} attack
+ * @property {number} attr0
+ * @property {number} attr1
+ * @property {number} crit
+ * @property {number} dex
+ * @property {number} dreturn
+ * @property {number} evasion
+ * @property {number} gold
+ * @property {number} hp
+ * @property {number} int
+ * @property {number} level
+ * @property {number} lifesteal
+ * @property {number} mp
+ * @property {number} range
+ * @property {number} reflection
+ * @property {number} resistance
+ * @property {number} rpiercing
+ * @property {number} speed
+ * @property {number} stat
+ * @property {number} str
+ * @property {number} vit
+ */
+
+/**
+ *  @typedef  {Object}  Character
+ *  @property {number}  hp               - health points
+ *  @property {number}  max_hp           - maximum health points
+ *  @property {number}  mp               - mana points
+ *  @property {number}  max_hp           - maximum mana points
+ *  @property {number}  xp               - current experience points
+ *  @property {number}  max_xp           - total experience points needed for next level
+ *  @property {number}  level            - character level
+ *  @property {string}  name             - character name
+ *  @property {string}  owner            - character owner //TODO need clarification
+ *  @property {number}  mp_cost          - mana cost for basic attack
+ *  @property {number}  range            - range for basic attack
+ *  @property {number}  resistance       - the character damage resistance
+ *  @property {number}  ref_speed        - walking speed
+ *  @property {number}  attack           - roughly estimated amount of damage
+ *  @property {boolean} afk              - is the character afk
+ *  @property {Array}   items            -
+ *  @property {number}  angle            - angle the character is looking at.
+ *  @property {number}  real_x           - x position on map
+ *  @property {number}  real_y           - y position on map
+ *  @property {number}  going_x          - the last target x position of the character
+ *  @property {number}  going_y          - the last target y position of the character
+ *  @property {number}  from_x           - the last movement starting x position of the character
+ *  @property {number}  from_y           - the last movement starting y position of the character
+ *  @property {number}  gold             - the amount of gold the character carrying
+ *  @property {boolean} moving           - is character moving
+ *  @property {boolean} afk              - player is afk
+ *  @property {boolean} rip              - is the character dead
+ *  @property {boolean} code              - is the character controled by code
+ *  @property {string}  target           - EntityId
+ *  @property {string}  type             - the type of the Entity
+ *
+ */
