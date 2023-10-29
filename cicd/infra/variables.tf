f827327c64b20c7bdc7078c27eec0f6780aaa70a
NexusNull <p.wellershaus@googlemail.com>
2023-10-29 18:18:41 +0100

    fix variables

--- cicd/infra/variables.tf
+++ cicd/infra/variables.tf
@@ -1,13 +1,3 @@
 variable "hcloud_token" {
   sensitive = true
 }
-variable "state_password" {
-  sensitive = true
-}
-variable "state_username" {
-  sensitive = true
-}
-
-variable "state_address" {
-  sensitive = true
-}
