f8e029ba8d4401afa8bfd4b743fc3d65ee963833
NexusNull <p.wellershaus@googlemail.com>
2023-11-23 15:24:11 +0100

    recreate secrets

--- cicd/infra/secrets.tf
+++ cicd/infra/secrets.tf
@@ -1,19 +1,19 @@
 resource "random_password" "bot_key" {
-  length  = 32
+  length  = 33
   special = false
 }
 
 resource "random_password" "server_master" {
-  length  = 32
+  length  = 33
   special = false
 }
 
 resource "random_password" "master" {
-  length  = 32
+  length  = 33
   special = false
 }
 
 resource "random_password" "keyword" {
-  length  = 32
+  length  = 33
   special = false
 }
