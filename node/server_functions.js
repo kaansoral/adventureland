99a359433c11c1d7d12f21009cd4ecb11250f648
thmsndk <9084377+thmsndk@users.noreply.github.com>
2023-12-14 01:30:14 +0100

    When Fishing or Mining something that uses `open` keep the Fishing or Mining phrase

--- node/server_functions.js
+++ node/server_functions.js
@@ -3452,7 +3452,15 @@ function exchange(player, name, args) {
 					bundle: drop[1] == "cxbundle",
 				});
 			} else if (drop[1] == "open") {
-				exchange(player, drop[2]);
+				switch (args.phrase) {
+					case "Fished":
+						args.phrase = `Fished ${drop[2]} and got`;
+						break;
+					case "Mined":
+						args.phrase = `Mined ${drop[2]} and got`;
+						break;
+				}
+				exchange(player, drop[2], args);
 			} else {
 				var item = create_new_item(drop[1], drop[2]);
 				var prop = undefined;
