48b64809e280d2bb88f6976674f74b10ae36f575
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2024-02-14 21:42:09 -0500

    Pass computer type to render_computer_network

--- js/functions.js
+++ js/functions.js
@@ -1242,9 +1242,9 @@ function inventory_click(num,event)
 		else if(character.items[num])
 		{
 			if(character.items[num].name=="placeholder") return;
-			if(character.items[num].name=="computer") // gameplay=="hardcore" && 
+			if(character.items[num].name=="computer" || character.items[num].name=="supercomputer") // gameplay=="hardcore" && 
 			{
-				return render_computer_network(".inventory-item");
+				return render_computer_network(".inventory-item",character.items[num].name);
 			}
 			render_item(".inventory-item",{id:"citem"+num,item:G.items[character.items[num].name],name:character.items[num].name,actual:character.items[num],num:num,inventory_ui:num});
 			last_invclick=num+iname;
