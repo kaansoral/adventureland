9c054b9594bb25f3caa0f92a8ce282a4b1d81044
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-04-20 23:18:47 +0200

    Avengers Shield does 10% of attack as damage

--- design/skills.py
+++ design/skills.py
@@ -961,11 +961,11 @@ skills={
 		"class":["paladin"],
 		"slot": [["offhand","shield"]],
 		"name":"Avengers Shield",
-		"explanation":"Hurls your shield at an enemy target, dealing (66.31% of Attack power) Holy damage, interrupting and silencing the non-Player target for 3 sec, and then jumping to 2 additional nearby enemie", 
+		"explanation":"Hurls your shield at an enemy target, dealing (10% of Attack power) Holy damage, interrupting and silencing the non-Player target for 3 sec, and then jumping to 2 additional nearby enemie", 
+		"attack_multiplier": 0.10, # 10% of Attack power
 		"range":200, # range multiplier of character range?
 		"mp":40,
 		"cooldown":3000,
-		"damage":1,
 		"target":True,
 		"hostile":True,
 		"projectile":"shield", # would be cool if this could adapt to the equipped shield
