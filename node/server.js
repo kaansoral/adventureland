8bb8645a307f5485d8465bc1dc1bcbcfdf99fb6c
FreezePhoenix <24441367+FreezePhoenix@users.noreply.github.com>
2024-06-19 12:42:42 -0400

    Change firecrackers to require friendly players

--- node/server.js
+++ node/server.js
@@ -7862,7 +7862,11 @@ function init_io() {
 					);
 					for (var id in instances[player.in].monsters) {
 						var monster = instances[player.in].monsters[id];
-						if (monster.target && distance({ map: player.map, in: player.in, x: x, y: y }, monster) < 64) {
+						if (
+							monster.target &&
+							is_same(player, get_player(monster.target), 1) &&
+							distance({ map: player.map, in: player.in, x: x, y: y }, monster) < 64
+						) {
 							stop_pursuit(monster, { force: true, cause: "firecrackers" });
 						}
 					}
