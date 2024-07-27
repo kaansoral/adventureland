368f28848ee28fb925c13c26331108e6fde9f16f
Telokis <6382729+Telokis@users.noreply.github.com>
2024-07-27 14:30:06 +0200

    More portable create_python3_copy.js

--- scripts/create_python3_copy.js
+++ scripts/create_python3_copy.js
@@ -1,24 +1,81 @@
 // creates a python3 project to safely test a python3 environment [27/01/24]
 
-var path=require("path"),f=require(path.resolve(__dirname, "functions.js"));
+const { rm, mkdir, readdir, cp } = require("node:fs/promises");
+const { globSync, existsSync } = require("node:fs");
+const path = require(`node:path`);
 
-f.execs("rm -rf ~/deploy/python3/thegame");
-f.execs("mkdir ~/deploy/python3/thegame");
-//f.execs("cp -r ~/thegame/* ~/deploy/python3/thegame");
-f.execs("rsync -rv --exclude=.electron ~/thegame/* ~/deploy/python3/thegame"); // steamworkds_sdk is problematic to cp [11/02/19]
+// This variable is used to define the temporary folder in which we prepare the deployment.
+// Override DEPLOY_PREP_FOLDER to install wherever you want.
+// Default was ~/deploy/python3/thegame before my rewrite
+const DEPLOY_PREP_FOLDER = path.resolve(__dirname, `..`, `to_deploy`);
 
-f.execs("rm -rf ~/deploy/python3/thegame/node");
-f.execs("rm -rf ~/deploy/python3/thegame/scripts");
-f.execs("cp -f ~/deploy/python3/thegame/python3/app.yaml ~/deploy/python3/thegame/app.yaml");
-f.execs("cp -f ~/deploy/python3/thegame/python3/requirements.txt ~/deploy/python3/thegame/requirements.txt");
-f.execs("rm -rf ~/deploy/python3/thegame/lib");
-f.execs("mkdir ~/deploy/python3/thegame/lib");
-f.execs("rm -rf ~/deploy/python3/thegame/origin");
-f.execs("rm -rf ~/deploy/python3/thegame/electron");
-f.execs("find ~/deploy/python3/thegame/ -name '*.pxm' -delete");
+// Root of the current repository.
+const REPO_ROOT = path.resolve(__dirname, "..");
 
-f.execs("cp ~/deploy/python3/thegame/js/runner_functions.js ~/deploy/python3/thegame/htmls/contents/codes/runner_functions.js");
-f.execs("cp ~/deploy/python3/thegame/js/runner_compat.js ~/deploy/python3/thegame/htmls/contents/codes/runner_compat.js");
-f.execs("cp ~/deploy/python3/thegame/js/common_functions.js ~/deploy/python3/thegame/htmls/contents/codes/common_functions.js");
+async function main() {
+	console.log(`Removing ${DEPLOY_PREP_FOLDER}...`);
+	await rm(DEPLOY_PREP_FOLDER, { recursive: true, force: true });
 
-// f.write_file("~/deploy/python3/thegame/app.yaml",f.read_file("~/thegame/app.yaml").replace("application:","#application:").replace("version:","#version:"));
+	console.log(`Creating ${DEPLOY_PREP_FOLDER}...`);
+	await mkdir(DEPLOY_PREP_FOLDER, { recursive: true });
+
+	console.log(`Copying project files`);
+	const IGNORE_LIST = [
+		"node_modules",
+		"to_deploy",
+		"node",
+		"scripts",
+		"/lib/",
+		"origin",
+		"electron",
+		".vscode",
+		".git",
+	];
+
+	const files = await readdir(REPO_ROOT);
+	const filteredFiles = files.filter((file) => !IGNORE_LIST.includes(file));
+
+	await Promise.all(
+		filteredFiles.map((file) =>
+			cp(path.resolve(REPO_ROOT, file), path.resolve(DEPLOY_PREP_FOLDER, file), {
+				recursive: true,
+				filter: (src, dest) => !IGNORE_LIST.some((ignoredFile) => src.includes(ignoredFile)),
+			}),
+		),
+	);
+	await mkdir(`${DEPLOY_PREP_FOLDER}/lib`);
+
+	// The following line requires node v22 at least
+	const filesToDelete = globSync(`${DEPLOY_PREP_FOLDER}/**/*.pxm`);
+
+	// We remove .pxm files
+	await Promise.all(filesToDelete.map((file) => rm(file)));
+
+	// Define an array of file pairs to be copied
+	// Each inner array contains [source, destination] paths
+	const FILES_TO_COPY = [
+		["python3/app.yaml", "app.yaml"],
+		["python3/requirements.txt", "requirements.txt"],
+		["js/runner_functions.js", "htmls/contents/codes/runner_functions.js"],
+		["js/runner_compat.js", "htmls/contents/codes/runner_compat.js"],
+		["js/common_functions.js", "htmls/contents/codes/common_functions.js"],
+	];
+
+	console.log("Copying files to their proper places for deployment...");
+	await Promise.all(
+		FILES_TO_COPY.map(([src, dest]) =>
+			cp(path.resolve(DEPLOY_PREP_FOLDER, src), path.resolve(DEPLOY_PREP_FOLDER, dest)),
+		),
+	);
+
+	const secretFile = path.resolve(DEPLOY_PREP_FOLDER, "secrets.py");
+
+	if (!existsSync(secretFile)) {
+		console.log("Copying secrets.py to deploy file. Make sure to edit it properly!");
+		await cp(path.resolve(DEPLOY_PREP_FOLDER, "useful/template.secrets.py"), secretFile);
+	}
+
+	console.log("Everything is done!");
+}
+
+main();
