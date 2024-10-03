8e52f276339305feb928cd01535da7bfded436f4
Kaan Soral <kaansoral@gmail.com>
2024-10-03 22:05:33 +0300

    .luckm fix

--- config.py
+++ config.py
@@ -114,12 +114,13 @@ if is_production:
 	#maps["desertland"]["key"]="jayson_desertland_copy"
 	pass
 
-game_version=802
+game_version=803
 SALES=4+5+388+5101+125/20 #donation+manual+macos+steam+sales
 update_notes=[
     "Halloween Event!",
     "Last Update [October 1st]",
     "Merged PR's from Github live.",
+    ".luckm Fix"
 ]
 ip_to_subdomain={ #IMPORTANT: SPECIAL PAGE RULES ARE NEEDED: https://dash.cloudflare.com/b6f5a13bded5fdd273e4a1cd3777162d/adventure.land/page-rules - uss1 / eus1 was best
 	"35.187.255.184":"asia1",
