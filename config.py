3e02af6cb5af87ec52b183b4a278b23bc87c18bd
Kaan Soral <kaansoral@gmail.com>
2024-10-03 22:05:08 +0300

    Pre luckm fix

--- config.py
+++ config.py
@@ -114,7 +114,7 @@ if is_production:
 	#maps["desertland"]["key"]="jayson_desertland_copy"
 	pass
 
-game_version=801
+game_version=802
 SALES=4+5+388+5101+125/20 #donation+manual+macos+steam+sales
 update_notes=[
     "Halloween Event!",
