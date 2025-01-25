734b7a9746bf3db3cf61f48bb549bd81e3e71042
Crowns3bc <44653214+Crowns3bc@users.noreply.github.com>
2025-01-24 20:56:45 -0700

    Added Mob spawn location
    Added Sparkbot and Targetron spawn location

--- design/maps.py
+++ design/maps.py
@@ -1696,8 +1696,8 @@ maps={
 		"npcs":[
 		],
 		"monsters":[
-			# {"type":"Robot1","boundary":[-734,-394,-416,-157],"count":6,"grow":True},
-			# {"type":"Robot2","boundary":[-672,-394,-416,-120],"count":6,"grow":True},
+			{"type":"sparkbot","boundary":[-734,-394,-416,-157],"count":6,"grow":True},
+			{"type":"targetron","boundary":[-672,-394,-416,-120],"count":6,"grow":True},
 			# {"type":"TBD","boundary":[194,-550,648,-407],"count":6,"grow":True},
 			# {"type":"TBD","boundary":[-431,-646,-242,-528],"count":6,"grow":True},
 		],
